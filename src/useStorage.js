42fef03080a5d9d92d693d3456e309fef78e8227
durulkoca <durulkoca@gmail.com>
2024-01-09 15:54:49 +0300

    Update useStorage

--- src/useStorage.js
+++ src/useStorage.js
@@ -4,20 +4,14 @@ import storage from "./storage";
 
 function useStorage(...args) {
   const initialValue = args.pop();
-  const getValue = () => {
-    const item = storage.get(...args);
 
-    if (item) {
-      return item;
-    } else {
-      storage.set(...args, initialValue);
-      return initialValue;
-    }
-  };
-
-  const [state, setState] = useState(getValue());
+  const [state, setState] = useState(null);
 
   useEffect(() => {
+    if (storage.get(...args) === null) {
+      storage.set(...args, initialValue);
+    }
+
     function handleStorageChange() {
       const value = storage.get(...args);
       setState(value);
